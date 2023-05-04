a1b7444c2ac29b84090e715a80dd0e40b5cdfef8
Andy Botting <andy@andybotting.com>
2023-05-04 17:46:26 +1000

    Fix docker-run for virtualenv
    Change-Id: I67517e492e63bca8cb9d40917e0f4e9570ae49ef

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,10 +1,4 @@
-#!/bin/bash -x
-
-if [ -f /vault/secrets/secret_envs ]; then
-    echo "** Loading secrets from /vault/secrets/secret_envs **"
-    . /vault/secrets/secret_envs
-else
-    echo "** Secrets not found! **"
-fi
-
+#!/bin/sh
+[ -f /vault/secrets/secret_envs ] && . /vault/secrets/secret_envs
+export PATH="/venv/bin:$PATH"
 gunicorn --bind 0.0.0.0:8612 --access-logfile=- pollinate.wsgi:application
