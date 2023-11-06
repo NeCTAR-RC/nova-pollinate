03fecd4e53409ef70b6b793d959e301a54dfb1e1
Andy Botting <andy@andybotting.com>
2023-11-06 13:43:33 +1100

    Don't use stale vault client
    Turns out that the k8s token we use for Vault expires after 1 hour.
    
    Due to caching, we weren't creating a new vault client, and so
    eventually the token would expire, but the original vault client
    would continue to use the expired token.
    
    This commit just instantiates a new client each time the secrets
    are requested (outside of our 60s cache)
    
    Change-Id: I1cb39c8c4fe61a6550c523e14e0570c3f95faa36

--- pollinate/secrets.py
+++ pollinate/secrets.py
@@ -31,39 +31,36 @@ K8S_TOKEN_PATH = '/var/run/secrets/kubernetes.io/serviceaccount/token'
 class PollinateSecrets():
 
     def __init__(self):
-        self.vault_client = None
         self.secrets_fetched = None
         self.secrets = None
         self.k8s_token = None
 
     def _get_vault_client(self):
         """Authenticate to vault and return a client"""
-        if not self.vault_client:
-            # Config provided token (development)
-            if CONF.vault.token:
-                LOG.debug('Logging in to Vault at: %s with local token',
-                          CONF.vault.url)
-                self.vault_client = Client(
-                    url=CONF.vault.url, token=CONF.vault.token)
-            # Kubernetes token (production)
-            elif CONF.vault.role:
-                LOG.debug('Logging in to Vault at: %s with K8s token',
-                          CONF.vault.url)
-                client = Client(url=CONF.vault.url)
-                if not self.k8s_token:
-                    with open(K8S_TOKEN_PATH, 'r') as f:
-                        self.k8s_token = f.read()
-                Kubernetes(client.adapter).login(
-                    role=CONF.vault.role, jwt=self.k8s_token)
-                self.vault_client = client
-            else:
-                raise Exception('No Vault method configured')
-        return self.vault_client
+        # Config provided token (development)
+        if CONF.vault.token:
+            LOG.debug('Logging in to Vault at: %s with local token',
+                      CONF.vault.url)
+            return Client(
+                url=CONF.vault.url, token=CONF.vault.token)
+        # Kubernetes token (production)
+        elif CONF.vault.role:
+            LOG.debug('Logging in to Vault at: %s with K8s token',
+                      CONF.vault.url)
+            client = Client(url=CONF.vault.url)
+            if not self.k8s_token:
+                with open(K8S_TOKEN_PATH, 'r') as f:
+                    self.k8s_token = f.read()
+            Kubernetes(client.adapter).login(
+                role=CONF.vault.role, jwt=self.k8s_token)
+            return client
+        else:
+            raise Exception('No Vault method configured')
 
     def get_secrets(self):
         if not self.secrets_fetched or \
                 self.secrets_fetched < datetime.now() - timedelta(seconds=60):
-            LOG.debug('Using cached secrets')
+            LOG.debug('Fetching secrets from Vault')
             client = self._get_vault_client()
             self.secrets = client.secrets.kv.read_secret_version(
                 path=CONF.vault.path,
