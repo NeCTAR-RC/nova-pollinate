91f4d277da16fa6cd814d623b21447bbb612f8e9
Sam Morrison <sorrison@gmail.com>
2025-06-04 16:27:48 +1000

    Fixup typo in gunicorn args
    Change-Id: I2e50742503fde7c0de23b0ca72ab5e8b3475a369

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PATH="/venv/bin:$PATH"
-gunicorn --bind :8080 --access-logfile=- --worker-tmp-dir /dev/shm --forwarded-allow-ips '*' --access_log_format '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' --workers 3 pollinate.wsgi:application
+gunicorn --bind :8080 --access-logfile=- --worker-tmp-dir /dev/shm --forwarded-allow-ips '*' --access-logformat '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' --workers 3 pollinate.wsgi:application
